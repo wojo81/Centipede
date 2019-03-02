e419cd34ff7953d235615ade2305ba1f435a35d8
wojo81 <tylerwojo81@gmail.com>
2019-03-02 09:47:59 -0600

    created window

--- centipede.py
+++ centipede.py
@@ -1,7 +1,15 @@
-import tkinter
+from tkinter import *
 
 def main():
-    pass
+    master= Tk()
+
+    screen_width= master.winfo_screenwidth()
+    screen_height= master.winfo_screenheight()
+
+    canvas= Canvas(master, width= screen_width, height= screen_height)
+    canvas.pack()
+
+    mainloop()
 
 if __name__ == "__main__":
     main()
